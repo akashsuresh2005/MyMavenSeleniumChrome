66dd51038393470981b3d6ac937932fc15a3ea4e
akashsuresh2005 <akashraghvendra2654@gmail.com>
2026-03-16 15:24:00 +0530

    Saved today

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,37 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WindowType;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
 
-        // Run SauceDemo Login Test
-        runSauceDemoTest();
-
-        // Run PracticeTestAutomation Login Test
-        runPracticeTestAutomation();
-
-        // Run AutomationExercise Add-To-Cart Test
-        runAutomationExerciseTest();
-    }
-
-
-
-    // ---------------------------------------------------
-    // Website 1 : https://www.saucedemo.com/
-    // Login using standard_user credentials
-    // ---------------------------------------------------
-    public static void runSauceDemoTest() throws InterruptedException {
-
-        System.out.println("Running SauceDemo Login Test");
-
         WebDriver driver = new ChromeDriver();
-
-        driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
+
+        // ---------------------------------------------------
+        // TAB 1 : SauceDemo
+        // ---------------------------------------------------
+        driver.get("https://www.saucedemo.com/");
 
         Thread.sleep(2000);
 
@@ -50,23 +33,14 @@ public class App {
 
         Thread.sleep(4000);
 
-        driver.quit();
-    }
 
 
-
-    // ---------------------------------------------------
-    // Website 2 : https://practicetestautomation.com/
-    // Login using student credentials
-    // ---------------------------------------------------
-    public static void runPracticeTestAutomation() throws InterruptedException {
-
-        System.out.println("Running PracticeTestAutomation Login");
-
-        WebDriver driver = new ChromeDriver();
+        // ---------------------------------------------------
+        // TAB 2 : PracticeTestAutomation
+        // ---------------------------------------------------
+        driver.switchTo().newWindow(WindowType.TAB);
 
         driver.get("https://practicetestautomation.com/practice-test-login/");
-        driver.manage().window().maximize();
 
         Thread.sleep(2000);
 
@@ -82,52 +56,37 @@ public class App {
 
         Thread.sleep(4000);
 
-        driver.quit();
-    }
 
 
-
-    // ---------------------------------------------------
-    // Website 3 : https://automationexercise.com/
-    // Scroll → Add first product to cart → Continue shopping
-    // ---------------------------------------------------
-    public static void runAutomationExerciseTest() throws InterruptedException {
-
-        System.out.println("Running AutomationExercise Add-To-Cart Test");
-
-        WebDriver driver = new ChromeDriver();
+        // ---------------------------------------------------
+        // TAB 3 : AutomationExercise
+        // ---------------------------------------------------
+        driver.switchTo().newWindow(WindowType.TAB);
 
         driver.get("https://automationexercise.com/");
-        driver.manage().window().maximize();
 
         Thread.sleep(4000);
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        // Scroll down to product section
         js.executeScript("window.scrollBy(0,600)");
 
         Thread.sleep(3000);
 
-        // Locate first Add-to-cart button
         WebElement addToCart = driver.findElement(By.xpath("(//a[@class='btn btn-default add-to-cart'])[1]"));
 
-        // Scroll button into view
         js.executeScript("arguments[0].scrollIntoView(true);", addToCart);
 
         Thread.sleep(2000);
 
-        // Click Add-to-cart using JavaScript (avoids intercepted click error)
         js.executeScript("arguments[0].click();", addToCart);
 
         Thread.sleep(3000);
 
-        // Click Continue Shopping in popup
         driver.findElement(By.xpath("//button[contains(text(),'Continue Shopping')]")).click();
 
-        Thread.sleep(4000);
+        Thread.sleep(5000);
 
         driver.quit();
     }
-
 }
